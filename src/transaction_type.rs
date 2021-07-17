3dc8c92440226b3b24cef02791cab3510a7b2443
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-17 09:29:41 -0700

    make enumeration comparable.

--- src/transaction_type.rs
+++ src/transaction_type.rs
@@ -1,6 +1,6 @@
 use serde::{ Deserialize, Serialize };
 
-#[derive(Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[serde(rename_all = "lowercase")]
 pub enum TransactionType {
     DEPOSIT,
