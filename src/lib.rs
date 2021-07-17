2b1287cbd0c886518ff73bcad9586c464e2c1526
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-17 13:51:27 -0700

    fix problem with test.

--- src/lib.rs
+++ src/lib.rs
@@ -9,25 +9,14 @@ mod tests {
     use crate::transaction_type::TransactionType;
     use chrono::prelude::*;
     use ordered_float::OrderedFloat;
+    use pretty_assertions::{assert_eq};
 
     use serde_json;
     #[test]
     fn serialize_record() {
         let record = Record::from(String::from("FF04C3DC-F0FE-472E-8737-0F4034C049F0"), Transaction::from(Local.ymd(2021, 7, 8).and_hms(0, 0, 0), Some(1260 as u32), String::from("Sam Hill Credit Union"), String::from("Open Account"), OrderedFloat::<f64>(500 as f64), TransactionType::DEPOSIT, false), None);
 
-        let expected_string = r#"
-        {
-            "id" : "FF04C3DC-F0FE-472E-8737-0F4034C049F0",
-            "transaction" : {
-              "amount" : 500,
-              "vendor" : "Sam Hill Credit Union",
-              "memo" : "Open Account",
-              "check_number" : 1260,
-              "type" : "deposit",
-              "date" : "2021-07-08"
-            }
-          }
-        "#;
+        let expected_string = "{\n  \"id\": \"FF04C3DC-F0FE-472E-8737-0F4034C049F0\",\n  \"transaction\": {\n    \"date\": \"2021-07-08\",\n    \"check_number\": 1260,\n    \"vendor\": \"Sam Hill Credit Union\",\n    \"memo\": \"Open Account\",\n    \"amount\": 500.0,\n    \"type\": \"deposit\"\n  }\n}";
 
         if let Ok(json) = serde_json::to_string_pretty(&record) {
             assert_eq!(json, expected_string)
