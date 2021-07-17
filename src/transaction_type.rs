ef719798596c711f6cf5e300cc833e1c5c0f591d
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-17 08:20:51 -0700

    make sure enumeration can be equal.

--- src/transaction_type.rs
+++ src/transaction_type.rs
@@ -1,6 +1,6 @@
 use serde::{ Deserialize, Serialize };
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "lowercase")]
 pub enum TransactionType {
     DEPOSIT,
