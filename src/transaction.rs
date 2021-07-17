02c00da9883315fb9d1de0af8bc2c73b5c1f393d
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-17 08:31:58 -0700

    continue working on main type.

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,11 +1,15 @@
 use serde::{ self, Serialize, Deserialize, Serializer, Deserializer };
 use crate::transaction_type::TransactionType;
 use chrono::prelude::*;
+use ordered_float::OrderedFloat;
 
+#[derive(Serialize, Deserialize)]
 pub struct Transaction {
     pub date: DateTime<Local>,
     pub check_number: Option<u32>,
     pub vendor: String,
     pub memo: String,
-    pub amount
+    pub amount: OrderedFloat<f64>,
+    #[serde(rename = "type")]
+    pub transaction_type: TransactionType
 }
