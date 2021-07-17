201dcb40c107bbf31a9cd3d89ff607b69cfcc2b0
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-17 15:11:07 -0700

    work on allowing records to easily be loaded.

--- src/record.rs
+++ src/record.rs
@@ -2,6 +2,8 @@ use crate::{ transaction::Transaction, transaction_type::TransactionType };
 use serde::{ Serialize, Deserialize };
 use uuid::Uuid;
 use ordered_float::OrderedFloat;
+use std::{fs::File, io::{ self, Read } };
+use serde_json;
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialOrd, Ord)]
 pub struct Record {
@@ -34,6 +36,18 @@ impl Record {
         }
     }
 
+    pub fn from_file(f: &str) -> Result<Vec<Record>, String> {
+        if let Ok(content) = file_contents_from(f) {
+            if let Ok(decoded_records) = serde_json::from_str::<Vec<Record>>(content) {
+                Ok(decoded_records)
+            } else {
+                Err(String::from("Could not parse data successfully."))
+            }
+        } else {
+            Err(String::from("Could not read file"))
+        }
+    }
+
     pub fn balance(&self) -> OrderedFloat<f64> {
         let mut value = OrderedFloat::from(0.0);
 
@@ -58,4 +72,11 @@ impl PartialEq for Record {
 
 fn default_id() -> String {
     Uuid::new_v4().to_hyphenated().to_string()
+}
+
+fn file_contents_from(f: &str) -> Result<String, io::Error> {
+        let mut file_content = String::new();
+        File::open(f)?.read_to_string(&mut file_content)?;
+
+        Ok(file_content)
 }
