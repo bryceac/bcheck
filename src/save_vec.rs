80dc30d463b95944581dc4061a4d4c37c46a6aaa
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-17 21:33:04 -0700

    add comments for trait.

--- src/save_vec.rs
+++ src/save_vec.rs
@@ -1,11 +1,18 @@
+// import custom type for use in specifying particular vector
 use crate::Record;
+
+// import things needed for dealing with files, so saving can work.
 use std::{fs::File, io::{ Write, Error } };
+
+// import serde_json, to save contents as JSON
 use serde_json;
 
+/// enables the ability to save data when implemented.
 pub trait Save {
     fn save(&self, path: &str) -> Result<(), Error>;
 }
 
+// add implementation of Save trait to Vector of Records.
 impl Save for Vec<Record> {
     fn save(&self, path: &str) -> Result<(), Error> {
         let mut output = File::create(path)?;
