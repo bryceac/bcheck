64ae0bbe465a9d48e544cfa0fca0ded9f994eee7
Bryce Campbell <tonyhawk2100@gmail.com>
2021-07-23 20:33:11 -0700

    fix matching.

--- src/transaction_type.rs
+++ src/transaction_type.rs
@@ -14,9 +14,9 @@ impl FromStr for TransactionType {
     type Err = TransactionTypeParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase() {
-            format!("deposit") => Ok(Self::Deposit),
-            format!("withdrawal") => Ok(Self::Withdrawal),
+        match s.to_lowercase().as_str() {
+            "deposit" => Ok(Self::Deposit),
+            "withdrawal" => Ok(Self::Withdrawal),
             _ => Err(TransactionTypeParseError::InvalidType(s.to_lowercase()))
         }
     }
