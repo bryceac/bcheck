bd7a7a64e096a021d6c08d057a703d9e0f2b26be
Bryce Campbell <tonyhawk2100@gmail.com>
2023-05-17 21:39:14 -0700

    create new trait for windows code.

---
+++ src/win_save_vec.rs
@@ -0,0 +1,37 @@
+// import custom type for use in specifying particular vector
+use crate::Record;
+
+// import things needed for dealing with files, so saving can work.
+use std::{fs::File, io::{ Write, Error } };
+
+// import serde_json, to save contents as JSON
+use serde_json;
+
+/// enables the ability to save data when implemented.
+pub trait WinSave {
+    fn save(&self, path: &str) -> Result<(), Error>;
+    fn save_tsv(&self, path: &str) -> Result<(), Error>;
+}
+
+// add implementation of Save trait to Vector of Records.
+impl WinSave for Vec<Record> {
+    fn save(&self, path: &str) -> Result<(), Error> {
+        let mut output = File::create(path)?;
+        let json_string = serde_json::to_string_pretty(self)?;
+
+        match write!(output, "{}", format!("{}", json_string)) {
+            Ok(()) => Ok(()),
+            Err(error) => Err(error)
+        }  
+    }
+
+    fn save_tsv(&self, path: &str) -> Result<(), Error> {
+        let mut output = File::create(path)?;
+        let tsv_string: String = self.iter().map(|record| record.to_string() + "\r\n").collect();
+
+        match write!(output, "{}", format!("{}", tsv_string)) {
+            Ok(()) => Ok(()),
+            Err(error) => Err(error)
+        } 
+    }
+}
